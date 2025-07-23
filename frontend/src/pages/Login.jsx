f2f2bc729d83184f66b2e48e97f38d78fb664471
chiheb-xhbb <chihebddine.selmi@etudiant-fst.utm.tn>
2025-07-23 12:03:51 +0100

    simple login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [motDePasse, setMotDePasse] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -13,33 +16,82 @@ const Login = () => {
         email,
         password: motDePasse,
       });
-      console.log("Login success:", response.data);
       localStorage.setItem("token", response.data.access_token);
-      navigate("/dashboard");// redirect to dashboard or home page
+      navigate("/dashboard");
     } catch (err) {
-      console.error(err.response?.data || err.message);
       setError(err.response?.data?.message || "Échec de connexion");
     }
-
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Mot de passe"
-        value={motDePasse}
-        onChange={(e) => setMotDePasse(e.target.value)}
-      />
-      <button type="submit">Login</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </form>
+    <section className="vh-100" style={{ backgroundColor: "#508bfc" }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card shadow-2-strong"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body p-5 text-center">
+                <h3 className="mb-5">Sign in</h3>
+                <form onSubmit={handleLogin}>
+                  <div className="mb-4 text-start">
+                    <label className="form-label" htmlFor="typeEmailX-2">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="typeEmailX-2"
+                      className="form-control form-control-lg"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="mb-4 text-start">
+                    <label className="form-label" htmlFor="typePasswordX-2">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="typePasswordX-2"
+                      className="form-control form-control-lg"
+                      value={motDePasse}
+                      onChange={(e) => setMotDePasse(e.target.value)}
+                      required
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="form-check d-flex justify-content-start mb-4">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="form1Example3"
+                    />
+                    <label
+                      className="form-check-label ms-2"
+                      htmlFor="form1Example3"
+                    >
+                      Remember password
+                    </label>
+                  </div>
+                  {error && <div className="alert alert-danger">{error}</div>}
+                  <button
+                    className="btn btn-primary btn-lg btn-block w-100"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                  <hr className="my-4" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
